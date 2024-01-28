c2c051f1093aa8d936d446107f66748e65fe1829
svilupp <49557684+svilupp@users.noreply.github.com>
2024-01-28 10:13:38 +0000

    up

--- examples/yi_quant_benchmark.jl
+++ examples/yi_quant_benchmark.jl
@@ -8,7 +8,7 @@ using PromptingTools
 const PT = PromptingTools
 
 # ## Run for a single test case
-device = "NVIDIA-RTX-4090" # "Apple-MacBook-Pro-M1" or "NVIDIA-GTX-1080Ti", broadly "manufacturer-model"
+device = "NVIDIA-RTX-4090-4x" # "Apple-MacBook-Pro-M1" or "NVIDIA-GTX-1080Ti", broadly "manufacturer-model"
 
 # Select models to run
 #
@@ -24,17 +24,17 @@ model_options = [
 
 # Or OSS models:
 model_options = [
-    # "yi:34b-chat-fp16",
-    # "yi:34b-chat-q8_0",
-    # "yi:34b-chat-q6_K",
-    # "yi:34b-chat-q5_K_M",
-    # "yi:34b-chat-q5_K_S",
-    # "yi:34b-chat-q5_0",
-    "yi:34b-chat-q4_K_M",
-    "yi:34b-chat-q4_0",
-    "yi:34b-chat-q3_K_L",
-    "yi:34b-chat-q3_K_S",
-    "yi:34b-chat-q2_K",
+    "yi:34b-chat-fp16",
+    "yi:34b-chat-q8_0",
+    "yi:34b-chat-q6_K",
+    "yi:34b-chat-q5_K_M",
+    "yi:34b-chat-q5_K_S",
+    "yi:34b-chat-q5_0",
+    # "yi:34b-chat-q4_K_M",
+    # "yi:34b-chat-q4_0",
+    # "yi:34b-chat-q3_K_L",
+    # "yi:34b-chat-q3_K_S",
+    # "yi:34b-chat-q2_K",
 ]
 
 # Select prompt templates to run (for reference check: `aitemplates("Julia")`)
@@ -64,27 +64,22 @@ schema_lookup = Dict{String, Any}([
 ] .=> Ref(PT.OllamaSchema()))
 
 # ## Run Benchmark - High-level Interface
-fn_definitions = find_definitions("code_generation/utility_functions")
+fn_definitions = find_definitions("code_generation")
 
 # or if you want only one test case:
 # fn_definitions = [
 #     joinpath("code_generation", "utility_functions", "clean_column", "definition.toml"),
 # ]
-# 4x JuliaRecapTask
-# JuliaRecapCoTTask - wrap_string - 5x
-#
-# 1x clean_column           JuliaExpertAsk 
-# JuliaExpertCoTTask - clean column 5x
 # num_gpu = floor(Int, 21 / 65 * 60)
 
-evals = run_benchmark(; fn_definitions = fn_definitions[5:end],
+evals = run_benchmark(; fn_definitions,
     models = model_options,
     prompt_labels = prompt_options,
     experiment = "yi-quantization-effects-default",
     auto_save = true, verbose = true,
     device,
     save_dir = "yi-quantization-effects",
-    num_samples = 5, schema_lookup, http_kwargs = (; readtimeout = 60),
+    num_samples = 5, schema_lookup, http_kwargs = (; readtimeout = 1000),
     api_kwargs = (; options = (; num_gpu = 99)));
 
 # ## Quick Eval
