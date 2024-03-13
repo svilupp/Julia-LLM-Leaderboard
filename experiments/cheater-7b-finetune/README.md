64c295d7e6d316b932418bd7a70ba3ac3fa93271
J S <49557684+svilupp@users.noreply.github.com>
2024-03-13 21:22:09 +0000

    Add charts

--- experiments/cheater-7b-finetune/README.md
+++ experiments/cheater-7b-finetune/README.md
@@ -50,3 +50,38 @@ You can shut down vLLM server now.
 2. If your loss quickly starts increasing, your `learning_rate` is too high. Halve it.
 3. Watch your GPU utilization and GPU memory stats with `watch -n0.1 nvidia-smi`. If it's not close to 100%, you're wasting time :)
 4. Set LORA hyperparameters based on https://www.anyscale.com/blog/fine-tuning-llms-lora-or-full-parameter-an-in-depth-analysis-with-llama-2
+
+
+## Cheater-7b Results
+
+**Training dataset**
+- File: `leaderboard_code_gen_data_trainset11.jsonl`
+- It contains 11/14 test cases, the top 50 samples that scored full points (100 points).
+- The held-out test cases are `q_and_a_extractor`, `extract_julia_code`, and `pig_latinify`.
+
+**Overall Results**
+
+| Model               | Elapsed | Score | Score Std Deviation | Count Zero Score | Count Full Score | Cost Cents |
+|---------------------|---------|-------|---------------------|------------------|------------------|------------|
+|   cheater-7b.Q5_K_M |     2.1 |  86.7 |                26.3 |                5 |              265 |        0.0 |
+|          cheater-7b |     6.1 |  85.0 |                27.4 |               11 |              242 |        0.0 |
+|  gpt-4-1106-preview |    22.5 |  68.9 |                35.0 |               50 |              149 |       1.21 |
+| dolphin-2.6-mistral |     8.2 |  57.9 |                30.4 |               32 |               66 |        0.0 |
+
+**By test case**
+| name                  | cheater-7b | cheater-7b.Q5_K_M | dolphin-2.6-mistral | gpt-4-1106-preview |
+|-----------------------|------------|-------------------|---------------------|--------------------|
+|       timezone_bumper |       97.0 |             100.0 |                84.4 |               91.1 |
+|        FloatWithUnits |      100.0 |             100.0 |                82.0 |               67.9 |
+|          clean_column |       95.8 |             100.0 |                70.8 |               80.8 |
+|      count_model_rows |       96.0 |             100.0 |                51.5 |               87.9 |
+| weather_data_analyzer |       98.0 |              92.0 |                57.8 |               86.2 |
+|       keep_only_names |      100.0 |             100.0 |                48.5 |               81.2 |
+|           wrap_string |       89.5 |              85.0 |                67.4 |               87.4 |
+|       event_scheduler |       97.8 |             100.0 |                56.6 |               63.0 |
+|            ispersonal |      100.0 |             100.0 |                60.0 |               50.0 |
+|         add_yearmonth |       95.0 |             100.0 |                45.2 |               65.0 |
+|           audi_filter |       93.8 |             100.0 |                55.5 |               51.8 |
+|     q_and_a_extractor |       55.0 |              73.3 |                42.3 |               47.6 |
+|    extract_julia_code |       48.4 |              38.5 |                55.6 |               50.0 |
+|          pig_latinify |       23.5 |              25.0 |                32.8 |               54.8 |
--- experiments/cheater-7b-finetune/analyse_results.jl
+++ experiments/cheater-7b-finetune/analyse_results.jl
@@ -1,39 +1,111 @@
 
-# # Evaluate results
-using Pkg;
-Pkg.activate(Base.current_project());
+# # Evaluate results
 using JuliaLLMLeaderboard
 using PromptingTools
 const PT = PromptingTools
-using DataFramesMeta
-using Statistics
+using CairoMakie, AlgebraOfGraphics
+using MarkdownTables, DataFramesMeta
+using Statistics: mean, median, quantile, std;
 
-## Load data
-df = load_evals("cheater-7b"; max_history = 0)
-@chain df begin
+# ! Config
+SAVE_PLOTS = false
+DIR_RESULTS = @__DIR__
+
+# # Load data
+df = load_evals("experiments/cheater-7b-finetune"; max_history = 0)
+df_all = load_evals("code_generation"; max_history = 0)
+
+df_combined = @chain df begin
+    @rtransform :model = :model ==
+                         "cognitivecomputations/dolphin-2.6-mistral-7b-dpo-laser" ?
+                         "dolphin-2.6-mistral" : :model
+    vcat(_, @rsubset(df_all, :model=="gpt-4-1106-preview"), cols = :intersect)
+end
+
+# ## Model Comparison
+
+# Highest average score by model:
+fig = @chain df_combined begin
     @by [:model] begin
         :cost = mean(:cost)
         :elapsed = mean(:elapsed_seconds)
         :score = mean(:score)
     end
+    transform(_, names(_, Number) .=> ByRow(x -> round(x, digits = 1)), renamecols = false)
+    @orderby -:score
+    @aside local order_ = _.model
+    data(_) *
+    mapping(:model => sorter(order_) => "Model",
+        :score => "Avg. Score (Max 100 pts)") *
+    visual(BarPlot; bar_labels = :y,
+        label_offset = 0)
+    draw(;
+        axis = (limits = (nothing, nothing, 0, 100),
+            xticklabelrotation = 45,
+            title = "Cheater-7b Performance"))
 end
-@chain df begin
-    @rsubset :model != "cheater-7b"
-    @by [:name] begin
+SAVE_PLOTS && save(joinpath(DIR_RESULTS, "model-comparison.png"), fig)
+fig
+
+# Table:
+output = @chain df_combined begin
+    @by [:model] begin
+        :cost = mean(:cost)
+        :elapsed = mean(:elapsed_seconds)
+        :score = mean(:score)
+        :score_std_deviation = std(:score)
+        :count_zero_score = count(iszero, :score)
+        :count_full_score = count(==(100), :score)
+    end
+    transform(_,
+        [:elapsed, :score, :score_std_deviation] .=> ByRow(x -> round(x, digits = 1)),
+        renamecols = false)
+    @rtransform :cost_cents = round(:cost * 100; digits = 2)
+    select(Not(:cost))
+    @orderby -:score
+    rename(_, names(_) .|> unscrub_string)
+end
+## markdown_table(output, String) |> clipboard
+markdown_table(output)
+
+# ## Overview by Prompt Template
+
+# Bar chart with all paid models and various prompt templates
+fig = @chain df_combined begin
+    @by [:model, :name] begin
+        :cost = mean(:cost)
+        :elapsed = mean(:elapsed_seconds)
+        :score = mean(:score)
+        :score_median = median(:score)
+        :cnt = $nrow
+    end
+    @aside local average_ = @by(_, :name, :avg=mean(:score)) |>
+                            x -> @orderby(x, -:avg).name
+    data(_) *
+    mapping(:name => sorter(average_) => "Test Case",
+        :score => "Avg. Score (Max 100 pts)",
+        color = :model => "Model",
+        dodge = :model) * visual(BarPlot)
+    draw(;
+        figure = (size = (800, 600),),
+        axis = (xticklabelrotation = 45, title = "Test Case Comparison"))
+end
+SAVE_PLOTS && save(joinpath(DIR_RESULTS, "test-case-comparison.png"), fig)
+fig
+
+# Table:
+output = @chain df_combined begin
+    @by [:model, :name] begin
         :cost = mean(:cost)
         :elapsed = mean(:elapsed_seconds)
         :score = mean(:score)
     end
+    @aside average_ = @by _ :name :AverageScore=mean(:score) |> x -> round(x, digits = 1)
+    unstack(:name, :model, :score; fill = 0.0)
+    transform(_, names(_, Number) .=> ByRow(x -> round(x, digits = 1)), renamecols = false)
+    leftjoin(average_, on = :name)
+    @orderby -:AverageScore
+    select(Not(:AverageScore))
 end
-@chain df begin
-    @rsubset :model == "cheater-7b.Q5_K_M"
-    mean(_.score)
-end
-@chain df begin
-    @rsubset :model == "cheater-7b.Q5_K_M"
-    @by [:name] begin
-        :cost = mean(:cost)
-        :elapsed = mean(:elapsed_seconds)
-        :score = mean(:score)
-    end
-end
+## markdown_table(output, String) |> clipboard
+markdown_table(output)
