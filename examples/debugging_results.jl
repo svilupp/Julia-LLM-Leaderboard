0e82a983a412a7e5903bdbc49dcd083803550ed3
J S <49557684+svilupp@users.noreply.github.com>
2024-08-12 20:34:13 +0100

    fix string inclusion

--- examples/debugging_results.jl
+++ examples/debugging_results.jl
@@ -1,21 +1,50 @@
 # # Example how to inspect a certain run
 
 ## Imports
+using PromptingTools
+const PT = PromptingTools
 using JuliaLLMLeaderboard
+using JuliaLLMLeaderboard: load_conversation_from_eval, run_code_main,
+                           run_code_blocks_additive
 using DataFramesMeta
 
 # Load everything
-df = load_evals("code_generation"; max_history=1)
+df = load_evals("code_generation"; max_history = 0)
+# df = load_evals("debug"; max_history = 0)
 
 # Inspect some result
-row = @chain df begin
-    @rsubset :prompt_label == "InJulia" :name == "event_scheduler" :model == "mistral-small"
-end
+# row = @chain df begin
+#     @rsubset :prompt_label=="InJulia" :name=="event_scheduler" :model=="mistral-small"
+# end
+
+# select which row to inspect
+row_idx = 2
 
 # take the evaluation filename and use it to load up the conversation
-conv = row.filename[end] |> load_conversation_from_eval
+conv = df.filename[row_idx] |> load_conversation_from_eval
 edit(conv) # Opens a preview in VSCode, alternatively use `preview(conv)`
 
 # Check out the code parsing results and errors
-cb = AICode(last(conv); skip_unsafe=true)
+cb = AICode(last(conv); skip_unsafe = true)
 cb.error
+
+# load the definition
+definition = load_definition(joinpath(
+    "code_generation", "utility_functions", df.name[row_idx], "definition.toml"))["code_generation"]
+
+## Execute the function definition
+cb, debugs = run_code_main(last(conv); return_debug = true)
+
+## Execute the examples
+cnt, debugs = run_code_blocks_additive(
+    cb, definition["examples"]; return_debug = true, verbose = true)
+
+## Execute the unit tests
+cnt, debugs = run_code_blocks_additive(
+    cb, definition["unit_tests"]; return_debug = true, verbose = true)
+
+## Run evaluation 1shot
+evaluate_1shot(;
+    conversation = conv, fn_definition = "", definition = definition,
+    auto_save = false, model = "", prompt_label = "", schema = PT.OpenAISchema(),
+    return_debug = true, verbose = true)
